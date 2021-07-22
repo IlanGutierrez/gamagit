2a584fb65df73af09fa43a073d1fb5534f38ac82
IlanGutierrez <igutierrezsoares@gmail.com>
2021-07-21 22:53:06 -0300

    aula de react

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
+import { useHistory } from 'react-router-dom';
 import * as S from './styled';
 
 export default function Repositories() {
+    const history = useHistory();
+    const [repositories, setRepositories] = useState ([]);
+
+        useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        if(repositoriesName !==null) {
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else {
+            history.push('/')
+        }
+    },[]);
+
     return (
-        <>
+        <S.Container>
             <S.Title>Repositórios</S.Title >
             <S.List>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
+                { repositories.map(repository =>{
+                    return(
+                        <S.ListItem>Repositório: {repository}</S.ListItem>
+                    )
+                } )}
             </S.List>
-        </>
+            <S.LinkHome to="/">Voltar</S.LinkHome>
+        </S.Container>
     )
 }
 
--- src/pages/Repositories/styled.js
+++ src/pages/Repositories/styled.js
@@ -1,4 +1,11 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    max-width: 991px;
+    margin: 0 auto;
+`
 
 export const Title = styled.h1`
     text-align:center;
@@ -19,3 +26,15 @@ export const ListItem = styled .li`
     color:#fff;
     padding: .5rem;
 `
+
+
+export const LinkHome = styled(Link)`
+    display: block;
+    width: 4rem;
+    text-align: center;
+    margin: 2rem auto;
+    background-color: #000;
+    padding: .5rem 0;
+    color: #fff;
+    text-decoration: nome;
+`;
